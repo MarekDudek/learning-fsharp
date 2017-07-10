532b1d1b155254eaa49672f5ae6bfac19c59950d
Marek Dudek <marek.dudek@gmail.com>
2017-07-10 18:49:01 +0200

    Strings, boolean and numbers, to be continued

--- Examples.fs
+++ Examples.fs
@@ -1,8 +1,45 @@
 module Examples
 
-let message = "Hello World"
+// Character data
+
+let string = "Hello World"
+let verbatimString = @"I have a backslash: \ unescaped"
+let multilineString = """I have "double-quotes"
+and multiple lines"""
+
+let bytearrayString = "I'm stored as a bytearray"B
+
+let singleCharacter = 'c'
+
+// Boolean
+
+let boolean = true
+
+// Numbers
+
+//   Integers
+let hexadecimalInteger = 0xFF
+let octalInteger = 0o377
+let binaryInteger = 0b11111111
+
+//   Bytes
+let unsignedByte = 127y
+let unsignedByteNegative = -128y
+let signedByteMin = 0uy
+let signedByteMax = 255uy
 
 let main() =
-  printfn "%A" message
+  printfn "string: %A" string
+  printfn "verbatim string: %A" verbatimString
+  printfn "multi-line string: %A" multilineString
+  printfn "single character: %A" singleCharacter
+  printfn "boolean: %A" boolean
+  printfn "hexadecimal integer %A" hexadecimalInteger
+  printfn "octal integer %A" octalInteger
+  printfn "binary integer %A" binaryInteger
+  printfn "unsigned byte %A" unsignedByte
+  printfn "unsigned byte negative %A" unsignedByteNegative
+  printfn "signed byte (min) %A" signedByteMin
+  printfn "signed byte (max) %A" signedByteMax
 
 main()
